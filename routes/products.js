e09a73c8c7820fb4029ef53f1534754dfb50e97d
Emanuel Martin Zarate <emanuelmartin@gmail.com>
2022-06-02 00:54:02 -0300

    Agregado de metodos put y delete

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const products = []
+let products = []
 
 router.get('/', function(req, res, next) {
   res.json(products);
@@ -28,6 +28,25 @@ router.post('/', function(req, res, next) {
   let product = {"id":products.length +1, "title":title, "price": price, "thumbnail":thumbnail}
   products.push(product)
   res.json(product)
+});
+
+router.put('/', function(req, res, next) {
+  const { id, title, price, thumbnail} =req.body
+  const index = products.findIndex(product => product.id === id)
+
+  products[index].title = title
+  products[index].price = Number(price)
+  products[index].thumbnail = thumbnail
+
+  res.json(products[index])
+
+});
+
+router.delete('/:id', function(req, res, next) {
+  const { id } = req.params
+  let productsFiltrado = products.filter(item => item.id !== Number(id))
+  products = productsFiltrado;
+  res.json( {msg: `Se elimino el ${id} de la lista de productos`})
 });
 
 module.exports = router;
